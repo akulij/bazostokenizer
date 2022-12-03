29fc1c363638b7fa46cdc8760840c5e9439667bb
Akulij <aakulij@gmail.com>
2022-12-03 16:54:28 +0500

    close db connection on shutdown

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import asyncio
 from fastapi import FastAPI
 
 from app import api
-from app.db import migrate
+from app.db import migrate, close_db_connection
 
 app = FastAPI()
 
@@ -11,3 +11,7 @@ app.include_router(api.router, prefix="/api")
 @app.on_event("startup")
 async def run_migration():
     await migrate()
+
+@app.on_event("shutdown")
+async def close_db():
+    await close_db_connection()
